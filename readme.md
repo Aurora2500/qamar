d0f488fe622dec3a3e0fd1efe0966195cca7b0ee
Aurora Zuoris <programming.aurora@gmail.com>
2024-04-30 12:24:31 +0100

    packaging metadata

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Qamar
+
+Qamar is a simple and easy to use Python library
+for using Lua in Python.
--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import os
 
 def main():
 	src_files = ["src/" + f for f in os.listdir("src") if f.endswith(".c")]
+	with open("readme.md", "r") as f:
+		long_description = f.read()
 	setup(
 		name="qamar",
 		version="0.1.0",
 		description="A lua binding for python",
+		long_description=long_description,
 		author="Aurora Zuoris",
 		author_email="programming.aurora@gmail.com",
 		ext_modules=[
-			Extension("qamar", src_files, libraries=["lua"])
+			Extension("qamar", src_files,include_dirs=["src"] , libraries=["lua"])
 		])
 
 if __name__ == "__main__":
